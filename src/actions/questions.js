702efb615753ced9672978bd33ae7afae8fa19f9
Eleni Fatsea <efatsea@gmail.com>
2021-03-22 14:14:10 +0000

    Add redux library

---
+++ src/actions/questions.js
@@ -0,0 +1,18 @@
+export const ADD_QUESTION = "ADD_QUESTION"
+export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS"
+
+function addQuestion ( question ) {
+	return {
+		type: ADD_QUESTION,
+      	question,
+      	
+	}
+
+}
+
+export function receiveQuestions( questions ) {
+	return {
+    	type: RECEIVE_QUESTIONS,
+     	questions,
+    }
+}
