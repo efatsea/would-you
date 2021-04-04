f111ff974af98e9878f528bf5f050f0469c0d1c6
root <efatsea@gmail.com>
2021-04-04 06:08:55 +0000

    add in Navigation bar the username and the avatar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 
-
 class Navigation extends Component {
   	
-  	
 	render(){
-    
+    	const { users, authedUser } = this.props
+  		const userAvatar =  authedUser ? users[authedUser].avatarURL : null
+        const userName = authedUser ? users[authedUser].name : null
     	return(
         	<div>
      			<span> Home </span>
           		<span> New Question </span>
           		<span> Leader Board </span>
           		<span>
-          			{`Hello ${this.props.authedUser}`}
-					<span>
-						
-					</span>
+          			{`Hello ${userName}`}
+                    <img
+                      src={userAvatar}
+                      width = "50"
+                      heigth = "50"
+                    />
+					
 				</span>
           		
           	</div>
@@ -30,8 +33,6 @@ function mapStateToProps ({ authedUser, users }) {
 	return {
     	authedUser,
 		users,
-		
-		
     }
 }
 
