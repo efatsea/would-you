b79f2646d404dcd9b54b8da71073ae1e69d8f1fb
root <efatsea@gmai.com>
2021-05-12 15:56:18 +0000

    Add new pictures and make a readme file

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: '/images/snow.png',
+    avatarURL: '/images/snow.jpg',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionOne',
